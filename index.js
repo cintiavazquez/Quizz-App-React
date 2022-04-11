99a1426ab383f911c696f4210e376436c39a09d6
Cintia Vazquez <vazquezalvarezcintia@gmail.com>
2022-04-11 22:30:14 +0200

    fix little mistakes js files

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './main.jsx';
 
-import App from './App';
+import App from './Components/App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
@@ -12,3 +11,5 @@ root.render(
     <App />
   </StrictMode>
 );
+
+console.log("hi I'm index.js");
